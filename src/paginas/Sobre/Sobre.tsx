39a0e3cb2dc3d1637221b864875f705b6dc96ded
Cr7stian8 <cristianotropeiro@gmail.com>
2022-07-12 19:13:17 -0300

    Removendo decoração do texto da navbar

--- src/paginas/Sobre/Sobre.tsx
+++ src/paginas/Sobre/Sobre.tsx
@@ -6,29 +6,17 @@ function Sobre() {
     return (
         <>
             <Grid container className="caixa">
-                <Grid item xs={8}>
+                <Grid item xs={4} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt='teste' width="90%" />
+                </Grid>
+                <Grid item xs={12}>
                     <Box className="box-text-d">
                         <Typography variant='h5' className='titulo'>
-                            Quem somos?
+                            O Projeto
                         </Typography>
                         <Typography className='descricao'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur fugit, voluptas delectus voluptates eaque corrupti, tempore cum cupiditate pariatur facere alias ducimus quo possimus, quidem sed temporibus ad officiis?
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={4} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt='teste' width="90%" />
-                </Grid>
-                <Grid item xs={4} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt='teste' width="90%" />
-                </Grid>
-                <Grid item xs={8}>
-                    <Box className="box-text-e">
-                        <Typography variant='h5' className='titulo'>
-                            ODS Escolhida
-                        </Typography>
-                        <Typography className='descricao'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur fugit, voluptas delectus voluptates eaque corrupti, tempore cum cupiditate pariatur facere alias ducimus quo possimus, quidem sed temporibus ad officiis?
+                            A BotanicHouse® é uma plataforma e-commerce criada com o objetivo de conectar consumidores do eixo urbano das grandes cidades com os produtores rurais de plantas ornamentais, silvestres e alimentícias. (verificar categorias)
+                            Aqui você poderá navegar entre nossas categorias e escolher as espécies de plantas que mais lhe agrada e se adaptam ao seu ambiente. Com uma breve descrição, você facilmente saberá se sua escolha é adequada ao seu tipo de ambiente e receber sua muda direto em sua residência, contribuindo para um mundo mais verde e sustentável.
                         </Typography>
                     </Box>
                 </Grid>
