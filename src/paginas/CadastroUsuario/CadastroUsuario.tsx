16f3d574553151ff383f1bf06fa1fe379b28a4e0
unknown <joaovneres@outlook.com>
2022-07-16 12:06:23 -0300

    Estilização das páginas

--- src/paginas/CadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/CadastroUsuario/CadastroUsuario.tsx
@@ -131,12 +131,11 @@ function CadastroUsuario() {
         >
             <Grid
                 item
-                xs={5}
+                xs={4}
                 alignItems='center'
                 className='formulario2'
-                >
-                <Box
-                    paddingX={10}>
+            >
+                <Box>
                     <form className='p-40' onSubmit={onSubmit}>
                         <Typography
                             variant='h3'
@@ -148,48 +147,46 @@ function CadastroUsuario() {
                         >
                             Cadastrar
                         </Typography>
-                        <TextField
-                            value={usuario.nomeUsuario}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                            id="nome"
-                            label="nome"
-                            variant='outlined'
-                            name='nomeUsuario'
-                            margin='normal'
-                            fullWidth
-                        />
-                        <TextField
-                            value={usuario.emailUsuario}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                            id="usuario"
-                            label="e-mail"
-                            variant='outlined'
-                            name='emailUsuario'
-                            margin='normal'
-                            fullWidth
-                        />
-                        <TextField
-                            value={usuario.senhaUsuario}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                            id="senha"
-                            label="senha"
-                            variant='outlined'
-                            name='senhaUsuario'
-                            margin='normal'
-                            fullWidth
-                            type='password'
-                        />
-                        <TextField
-                            value={confirmarSenha}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}
-                            id="confirmarSenha"
-                            label="confirmar senha"
-                            variant='outlined'
-                            name='confirmarSenha'
-                            margin='normal'
-                            type='password'
-                            fullWidth
-                        />
+                        <Box className='containerForms'>
+                            <TextField
+                                value={usuario.nomeUsuario}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="nome"
+                                label="Nome"
+                                variant='outlined'
+                                name='nomeUsuario'
+                                margin='normal'
+                            />
+                            <TextField
+                                value={usuario.emailUsuario}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="usuario"
+                                label="E-mail"
+                                variant='outlined'
+                                name='emailUsuario'
+                                margin='normal'
+                            />
+                            <TextField
+                                value={usuario.senhaUsuario}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id="senha"
+                                label="Senha"
+                                variant='outlined'
+                                name='senhaUsuario'
+                                margin='normal'
+                                type='password'
+                            />
+                            <TextField
+                                value={confirmarSenha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}
+                                id="confirmarSenha"
+                                label="Confirmar senha"
+                                variant='outlined'
+                                name='confirmarSenha'
+                                margin='normal'
+                                type='password'
+                            />
+                        </Box>
                         <Box marginTop={2} className="botaoCU">
                             <Link to='/logar' className='text-decorator-none'>
                                 <Button
